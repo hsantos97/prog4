65c263c9e229ed5ad78548dd7a314d56df92da71
Harison Santos <harison_vr@hotmail.com>
2022-02-07 19:14:16 -0300

    introduction js

--- modulo3/lista1Mod3.js
+++ modulo3/lista1Mod3.js
@@ -1,3 +1,51 @@
+// A
+var a = 12;
+(function() {
+console.log(a);
+})();
+
+// B
+var a = 5;
+(function() {
+var a = 12;
+console.log(a);
+})();
+
+// C
+var a = 10;
+var x = (function() {
+var a = 12;
+return (function() {
+console.log(a);
+});
+})();
+x();
+
+// D
+var a = 10;
+var x = (function() {
+var y = function() {
+var a = 12;
+};
+return function() {
+console.log(a);
+}
+})();
+x();
+
+// E
+var a = 10;
+var x = (function() {
+(function() {
+a = 12;
+})();
+return (function() {
+console.log(a);
+});
+})();
+x();
+
+// F
 var a = 10;
 (function() {
     var a = 15;
