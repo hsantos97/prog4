379f66c3e76ce2ebeb25814b263dc58ce0778a96
Harison Santos <58005145+hsantos97@users.noreply.github.com>
2022-03-08 10:51:37 -0300

    Add files via upload
    lista 3 mod 3

---
+++ modulo3/listaProdutos.js
@@ -0,0 +1,50 @@
+let botao = document.getElementsByClassName('botao');
+let lista = document.getElementsByClassName('lista');
+let total = document.getElementsByClassName('total');
+let selecao = document.getElementsByClassName('selecao');
+
+function getHTML(id){
+    let html = document.getElementById(id).innerHTML;
+    document.getElementsByClassName('lista').innerHTML = html;
+}
+getHTML(OP1)
+
+var resultado = 0;
+
+function soma(){
+    resultado += selecao.value;  
+    console.log(selecao[0]);
+
+}
+
+
+function guardaLista(){
+    //alert('AQUI')
+    var valor = selecao.value
+    var listaItens = document.createElement('li')
+    if(valor == 10){
+        listaItens.innerHTML = 'OP1'
+    }
+    if(valor == 20){
+        listaItens.innerHTML = 'OP2'
+    }
+    if(valor == 30){
+        listaItens.innerHTML = 'OP3'
+    }
+    if(valor == 40){
+        listaItens.innerHTML = 'OP4'
+    }
+    if(valor == 50){
+        listaItens.innerHTML = 'OP5'
+    }
+    if(valor == 60){
+        listaItens.innerHTML = 'OP6'
+    }
+    total.innerHTML = '$${resultado},00'
+    lista.appendChild(listaItens)
+}
+
+botao.addEventListener('click', function(){
+    soma();
+    guardaLista();
+}, false)
